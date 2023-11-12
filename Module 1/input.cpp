eb583da7dcb2b6202999904f05758a0e106fc6a4
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-11-12 17:06:42 +0600

    input in cpp

---
+++ Module 1/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a, b;
+    // cin >> a >> b;
+    // cout << a << " " << b << endl;
+
+    char c;
+    cin >> c >> a;
+    cout << c << " " << a;
+}
