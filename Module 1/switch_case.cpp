bb43cec263c6fcfd87e97c3990677f69679424d1
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-11-12 19:46:06 +0600

    switch case initialize

---
+++ Module 1/switch_case.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+
+    switch (a)
+    {
+    case 1:
+        cout << "It's 1" << endl;
+        break;
+    case 2:
+        cout << "It's 2" << endl;
+        break;
+    case 3:
+        cout << "It's 3" << endl;
+        break;
+    case 4:
+        cout << "It's 4" << endl;
+        break;
+    default:
+        cout << "Halai Kamda korlo ki" << endl;
+    }
+    return 0;
+}
