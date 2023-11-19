7cafc1848b51ecb162fcdb30d6c92b6a7701bded
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-11-20 01:21:22 +0600

    string constructor

---
+++ Module 6/string_constructor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "hello world";
+    // 1
+    // string s("Hello World");
+    // 2
+    // string s("hello world", 7);
+    // 3
+    // string a = "Hello world";
+    // string s(a, 4);
+
+    string s(6, 'A');
+    cout << s << endl;
+    return 0;
+}
