37565111426b4df3ce905e21def98f608650349a
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-11-19 20:42:59 +0600

    modifiers built in function

---
+++ Module 5/modifiers.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string a = "Hello";
+    // string b = "Gello";
+
+    // a += b;
+    // b.append(a);
+    // cout << a << endl;
+    // cout << b << endl;
+
+    // string s = "Hello";
+    // // s = "Gello";
+    // s.assign("Marlo");
+    // cout << s << endl;
+
+    // string a = "HelloWorld";
+    // // a.erase(4);
+    // // a.erase(4, 1);
+    // a.replace(4, 3, "so");
+    // cout << a << endl;
+
+    // string s = "HelloWorld";
+    // // s.replace(5, 0, "Arko");
+    // s.insert(5, "AroArko");
+    // cout << s << endl;
+    return 0;
+}
