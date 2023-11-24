4f47b0d553933608e6b41465aa468b7ff8088819
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-11-24 23:25:39 +0600

    min max find from an obj

---
+++ Module 7/min_max_obj_find.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    Student a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+    Student mn;
+    Student mx;
+    mn.marks = INT_MAX;
+    mx.marks = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i].marks < mn.marks)
+        {
+            mn = a[i];
+        }
+        if (a[i].marks > mx.marks)
+        {
+            mx = a[i];
+        }
+    }
+    cout << "Min marks: " << endl;
+    cout << mn.name << " " << mn.roll << " " << mn.marks << endl;
+    cout << "Max Marks: " << endl;
+    cout << mx.name << " " << mx.roll << " " << mx.marks << endl;
+
+    return 0;
+}
