e2ada63d72fcf2c553799d08b9337bf00baad01e
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-11-25 15:11:06 +0600

    reverse it

---
+++ Module 8/Reverse_It.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int cls;
+    char s;
+    int id;
+};
+int main()
+{
+    int n;
+    cin >> n;
+    Student a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].cls >> a[i].s >> a[i].id;
+    }
+    // reverse(a, a + n);
+    for (int i = 0; i < n / 2; i++)
+    {
+        swap(a[i].s, a[n - i - 1].s);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].cls << " " << a[i].s << " " << a[i].id << endl;
+        ;
+    }
+
+    return 0;
+}
